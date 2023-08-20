b98375d90f49d2d6f0fc4f6983f67f503876fe7b
runweiting <whatisfuntime@gmail.com>
2023-08-20 14:44:36 +0800

    feat: import swiper

--- main.js
+++ main.js
@@ -1,4 +1,34 @@
+// 預設
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
+
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
+
+// import styles bundle
+import 'swiper/css/bundle';
+
+// init Swiper：宣告一個新的常數Swiper 指定在.swiper下有swiper的效果
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
 
 console.log("Hello world!");
