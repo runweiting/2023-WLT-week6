bf809df17ce1d7173722149300e9c527fc9581e5
runweiting <whatisfuntime@gmail.com>
2023-08-23 15:41:37 +0800

    feat: swiper跑馬燈樣式

--- main.js
+++ main.js
@@ -9,14 +9,29 @@ import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
 
 // init Swiper：宣告一個新的常數Swiper 指定在.swiper下有swiper的效果
+// 修正：學習每日任務 Day38-swiper跑馬燈樣式後
 const swiper = new Swiper('.swiper', {
-    slidesPerView: 'auto',
+  
+    //避免使用者移動到
+    allowTouchMove: false,
+    
+    //能呈現幾個.swiper-slide
+    slidesPerView: "auto",
+      
+    //跑馬燈的速度(毫秒)
     speed: 20000,
+    
+    //是否重複
     loop: true,
+    
     autoplay: {
-        delay: 1, // 過1秒才跑下一個
-      },
 
+    //到下一個幻燈片時，停下的時間
+    delay: 0,    
+    //避免點擊造成跑馬燈失效，視情況增加
+    disableOnInteraction: false
+
+    },
   });
 
 
