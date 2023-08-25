5bd781e2996d86c0ef48461418fc806ca6444219
runweiting <whatisfuntime@gmail.com>
2023-08-25 12:24:02 +0800

    feat: GSAP h5

--- main.js
+++ main.js
@@ -25,12 +25,10 @@ const swiper = new Swiper('.swiper', {
     loop: true,
     
     autoplay: {
-
-    //到下一個幻燈片時，停下的時間
-    delay: 0,    
-    //避免點擊造成跑馬燈失效，視情況增加
-    disableOnInteraction: false
-
+      //到下一個幻燈片時，停下的時間
+      delay: 0,
+      //避免點擊造成跑馬燈失效，視情況增加
+      disableOnInteraction: false
     },
   });
 
