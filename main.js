db9ce0c9ada90ef8d38a3056d79e13c74c5551fb
runweiting <whatisfuntime@gmail.com>
2023-09-05 12:30:42 +0800

    助教回饋: 修改選擇器 querySelectorAll

--- main.js
+++ main.js
@@ -29,8 +29,8 @@ window.gsap = gsap;
 const g_logo = document.querySelector(".g_logo");
 const g_searchbar = document.querySelector(".g_searchbar");
 const g_btnmenu = document.querySelector(".g_btnmenu");
-const g_sparkler = document.querySelector(".g_sparkler");
-const g_btnStatus = document.querySelector(".g_btnStatus");
+const g_sparkler = document.querySelectorAll(".g_sparkler");
+const g_btnStatus = document.querySelectorAll(".g_btnStatus");
 let tl = gsap.timeline(); //create the timeline
 
 gsap.to(g_logo,{
@@ -78,7 +78,29 @@ tl.to(g_btnStatus, {
   repeat: 10,
 }); 
 
-
+// aos
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+AOS.init({
+  // Global settings:
+  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: 'aos-init', // class applied after initialization
+  animatedClassName: 'aos-animate', // class applied on animation
+  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+  
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 1000, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+});
 
 
 
